1a4b81b8c15b37829091ef4be31f58228a3f56a9
Garrick Aden-Buie <garrick@adenbuie.com>
2025-08-29 10:16:11 -0400

    use pyhere

--- _exercises/06-word-games/06_word-games.py
+++ _exercises/06-word-games/06_word-games.py
@@ -1,21 +1,24 @@
+# %%
 import chatlas
 import random
-from pathlib import Path
+from pyhere import here
 from dotenv import load_dotenv
 
 load_dotenv()
 
-words_file = Path(__file__).parent / 'words.txt'
-words = words_file.read_text().strip().split('\n')
+words_file = here() / "_exercises" / "06-word-games" / "words.txt"
+words = words_file.read_text().strip().split("\n")
 
+# %%
 chat = chatlas.ChatOpenAI(
-  system_prompt=f"""
+    system_prompt=f"""
 We are playing a word guessing game.
 Here's the secret words: {random.sample(words, 1)}.
-Give the user an initial clue and then only answer their questions with yes or no.
+Give the user an initial riddle and then only answer their questions with yes or no.
 When they win, use lots of emojis.
   """
 )
 
-# Start the chat and say "Let's play"
+# %%
+print('Start the chat by saying "Let\'s play"')
 chat.console()
