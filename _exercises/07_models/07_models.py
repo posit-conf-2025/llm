7850dd50b80554f0f02c356938f509043fb9d3b9
Garrick Aden-Buie <garrick@adenbuie.com>
2025-09-08 12:03:04 -0400

    update exercise 07

--- _exercises/07_models/07_models.py
+++ _exercises/07_models/07_models.py
@@ -1,16 +1,35 @@
 # %%
-import chatlas
 import dotenv
+from chatlas import ChatAuto
 
 dotenv.load_dotenv()
 
-# %%
-chatlas.ChatOpenAI().list_models()
-[m["id"] for m in chatlas.ChatOpenAI().list_models()]
-
-chatlas.ChatAnthropic().list_models()
-chatlas.ChatOllama("gemma3").list_models()
+# %% [markdown]
+# List models by calling the `list_models` method on a `Chat` instance.
 
 # %%
-chat = chatlas.ChatOllama("gemma3:4b")
-chat.chat("Write a recipe for an easy weeknight dinner my kids would like")
+ChatAuto("openai").list_models()
+
+# %% [markdown]
+# You can also load the models into a Polars DataFrame for easier viewing.
+# Try it out with different providers: `"anthropic"`, `"ollama/gemma3"`.
+
+# %%
+import polars as pl
+
+models = ChatAuto("openai").list_models()
+models = pl.DataFrame(models)
+models
+
+# %% [markdown]
+# Now try sending the same prompt to different models to compare the responses.
+
+# %%
+prompt = "Write a  recipe for an easy weeknight dinner my kids would like."
+
+ChatAuto("openai/gpt-5").chat(prompt)
+ChatAuto("anthropic/claude-3-7-sonnet-20250219").chat(prompt)
+
+# %%
+# If you have local models installed, try them out with Ollama.
+ChatAuto("ollama/gemma3:4b").chat(prompt)
